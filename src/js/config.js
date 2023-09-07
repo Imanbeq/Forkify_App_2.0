d477813480b9862f26d6403b4f9c970df2df84c5
Qlove Iman <imanqlove@gamil.com>
2023-09-07 11:10:18 +0600

    initial coomit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '46e1d97d-2f01-440d-ad9d-87d0ee7c437d';
+export const MODAL_CLOSE_SEC = 1.5;
